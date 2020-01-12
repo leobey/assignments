2978ae4bfec775420f8ed0bb722777b9201e5fd3
leonie <leonie_beyrle@hks.harvard.edu>
2020-01-11 21:58:25 -0500

    add functions to calculator

---
+++ calculator.py
@@ -0,0 +1,15 @@
+#defining function to multiply 
+def multiply(a,b): 
+	return a*b 
+
+#defining function to add 
+def add(a,b):
+	return a+b
+
+#defining function to subtract 
+def subtract(a,b):
+	return a-b
+
+#defining function to divide 
+def divide (a,b):
+	return a/b 
