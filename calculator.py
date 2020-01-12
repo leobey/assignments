5c928d813e7fb8aca566e0ebc5c94564c45f2cf3
leonie <leonie_beyrle@hks.harvard.edu>
2020-01-11 22:03:05 -0500

    call multiply function and print result

--- calculator.py
+++ calculator.py
@@ -13,3 +13,7 @@ def subtract(a,b):
 #defining function to divide 
 def divide (a,b):
 	return a/b 
+
+print("I'm going to use the calculator functions to multiply 5 and 6")
+x = multiply(5,6)
+print(x)
