72e320821df3221941a8d409b428a10a463ea268
leonie <leonie_beyrle@hks.harvard.edu>
2020-01-12 16:33:22 -0500

    add functions to calculator

---
+++ calculator.js
@@ -0,0 +1,19 @@
+//defining function to multiply 
+function multiply(a,b){
+	return a*b
+} 
+
+//defining function to add 
+function add(a,b){
+	return a+b
+}
+
+//defining function to subtract 
+function subtract(a,b){
+	return a-b
+}
+
+//defining function to divide 
+function divide(a,b){
+	return a/b
+}
